9d992c7f832d94964b400ab230fb596191e0df57
dheidenr <work-post@bk.ru>
2020-05-03 00:54:54 +0300

    finish beta code coursera python home_task_2

--- coursera/python/week3/home_task_2/solution.py
+++ coursera/python/week3/home_task_2/solution.py
@@ -3,28 +3,40 @@ import csv
 
 
 class CarBase:
-    def __init__(self, car_type=None, brand=None,
+    car_type = ''
+
+    def __init__(self,
+                 # car_type=None,
+                 brand=None,
                  photo_file_name=None, carrying=None):
-        self.car_type = car_type
+        # self.car_type = car_type
         self.brand = brand
         self.photo_file_name = photo_file_name
         try:
             self.carrying = float(carrying)
         except TypeError:
-            self.carrying = None
+            self.carrying = 0.0
 
     def get_photo_file_ext(self):
-        return os.path.splitext(self.photo_file_name)
+        ext = os.path.splitext(self.photo_file_name)[1]
+        if ext == '' or ext not in ['.jpg', '.jpeg', '.png', '.gif']:
+            raise ValueError
+        return os.path.splitext(self.photo_file_name)[1]
 
 
 class Car(CarBase):
-    def __init__(self, car_type, brand, photo_file_name, carrying,
+    def __init__(self,
+                 # car_type,
+                 brand, photo_file_name, carrying,
                  passenger_seats_count=None):
-        super().__init__(car_type, brand, photo_file_name, carrying)
+        super().__init__(
+            # car_type,
+            brand, photo_file_name, carrying)
         try:
             self.passenger_seats_count = int(passenger_seats_count)
         except TypeError:
-            self.passenger_seats_count = None
+            self.passenger_seats_count = 0
+        self.__class__.car_type = 'car'
 
 
 class Truck(CarBase):
@@ -32,50 +44,107 @@ class Truck(CarBase):
     body_width = None
     body_height = None
 
-    def __init__(self, car_type, brand, photo_file_name, carrying,
-                 body_whl: str):
-        super().__init__(car_type, brand, photo_file_name, carrying)
+    def __init__(self,
+                 # car_type,
+                 brand, photo_file_name, carrying,
+                 body_whl):
+        super().__init__(
+            # car_type,
+            brand, photo_file_name, carrying)
         try:
             whl = list(map(lambda x: float(x), body_whl.split('x')))
-            self.body_width = whl[0]
-            self.body_height = whl[1]
-            self.body_length = whl[2]
+            if len(whl) > 3:
+                raise IndexError
+            self.body_width = whl[1]
+            self.body_height = whl[2]
+            self.body_length = whl[0]
         except (IndexError, ValueError):
             self.body_width, self.body_height, self.body_length = 0.0, 0.0, 0.0
+        self.__class__.car_type = 'truck'
 
     def get_body_volume(self):
         return self.body_width * self.body_height * self.body_length
 
 
 class SpecMachine(CarBase):
-    def __init__(self, car_type, brand, photo_file_name, carrying, extra=None):
-        super().__init__(brand, car_type, photo_file_name, carrying)
+    def __init__(self,
+                 # car_type,
+                 brand, photo_file_name, carrying, extra=None):
+        super().__init__(
+            # car_type,
+            brand, photo_file_name, carrying)
         self.extra = extra
+        self.__class__.car_type = 'spec_machine'
+
+
+class ConstructorCar:
+    def __init__(self, car_type, brand, passenger_seats_count, photo_file_name,
+                 body_whl, carrying, extra):
+        pass
 
 
 def get_car_list(csv_filename):
-    file = open(csv_filename, "r")
-    print(file)
+    try:
+        file = open(csv_filename, "r")
+    except IOError:
+        return []
     reader = csv.DictReader(file, delimiter=';')
+    # print(reader.__dict__)
+    # if reader.line_num == 0:
+    #     return []
     car_list = []
     for line in reader:
-        print(line)
+        if line["car_type"] == '' or line["brand"] == '' \
+                or line["photo_file_name"] == '' or line["carrying"] == '' \
+                or line["car_type"] not in ['car', 'truck', 'spec_machine']:
+            continue
+        try:
+            if line["carrying"] != '':
+                float_test = float(line['carrying'])
+        except TypeError:
+            continue
+        ext = os.path.splitext(line["photo_file_name"])[1]
+        if ext == '' or ext not in ['.jpg', '.jpeg', '.png', '.gif']:
+            continue
         if line["car_type"] == 'car':
-            car_list.append(Car(line["car_type"],
-                                line["brand"],
-                                line["photo_file_name"],
-                                line['carrying'],
-                                line['passenger_seats_count']))
+            try:
+                int(line["passenger_seats_count"])
+                if line["passenger_seats_count"] == '':
+                    raise TypeError
+            except (TypeError, ValueError):
+                continue
+            car = Car(
+                # line["car_type"],
+                line["brand"],
+                line["photo_file_name"],
+                line['carrying'],
+                line['passenger_seats_count'])
+            car.car_type = line["car_type"]
+            car_list.append(car)
+
+
         elif line["car_type"] == 'truck':
-            car_list.append(Truck(line["car_type"],
-                                  line["brand"],
-                                  line["photo_file_name"],
-                                  line['carrying'],
-                                  line['body_whl']))
+            # if line['body_whl'] == '':
+            #     continue
+            car = Truck(
+                # line["car_type"],
+                line["brand"],
+                line["photo_file_name"],
+                line['carrying'],
+                line['body_whl'])
+            car.car_type = line["car_type"]
+            car_list.append(car)
         elif line["car_type"] == 'spec_machine':
-            car_list.append(SpecMachine(line["car_type"],
-                                        line["brand"],
-                                        line["photo_file_name"],
-                                        line['carrying'],
-                                        line['extra']))
+            if line['extra'] == '':
+                continue
+            car = SpecMachine(
+                # line["car_type"],
+                line["brand"],
+                line["photo_file_name"],
+                line['carrying'],
+                line['extra'])
+            car.car_type = line["car_type"]
+            car_list.append(car)
+    if reader.fieldnames is None:
+        return []
     return car_list
--- coursera/python/week3/home_task_2/test.py
+++ coursera/python/week3/home_task_2/test.py
@@ -18,12 +18,17 @@ if __name__ == '__main__':
     # print(l[50])
     cars = get_car_list('coursera_week3_cars.csv')
     print(len(cars))
+    print(cars)
     for car in cars:
-        print(type(car.carrying))
+        if isinstance(car, Truck):
+            print(f"truck, w{car.body_width}, h{car.body_height}, l{car.body_length}")
         print(type(car))
+        print(type(car.car_type))
+    print('end')
+    # print(cars[0].get_photo_file_ext())
 
-    print(cars[0].passenger_seats_count)
-    print(cars[1].get_body_volume())
-    print(cars[0].car_type)
-    print(cars[4].extra)
-    print(cars[2].body_width)
+    # print(cars[0].passenger_seats_count)
+    # print(cars[1].get_body_volume())
+    # print(cars[0].car_type)
+    # print(cars[4].extra)
+    # print(cars[2].body_width)
